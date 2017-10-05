8efa9d4af08f9af8bcf55ff34a2bdc4e40fb2190
Kenneth <kennethkh96@gmail.com>
2017-10-05 12:18:10 +0200

    Added ddl

---
+++ BudgetManager/Bilag/Database/DDL.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE BudgetManager
+GO
+USE BudgetManager
+GO
+CREATE TABLE Visibility
+(
+	Id INT IDENTITY(1,1) PRIMARY KEY,
+	Title NVARCHAR(100),
+	[Description] NVARCHAR(255)
+)
+CREATE TABLE Interval
+(
+	IntervalTitle NVARCHAR(100) PRIMARY KEY,
+	IntervalValue INT
+)
+
+CREATE TABLE Budgets
+(
+	Id INT IDENTITY(1,1) PRIMARY KEY,
+	Purpose NVARCHAR(255),
+	AccountYear NVARCHAR(4),
+	Fk_VisbilityId INT NOT NULL,
+	BudgetTitle NVARCHAR(100),
+	FK_IntervalTitle NVARCHAR(100),
+
+	FOREIGN KEY (Fk_VisbilityId) REFERENCES Visibility(Id),
+	FOREIGN KEY (FK_IntervalTitle) REFERENCES Interval(IntervalTitle),
+)
