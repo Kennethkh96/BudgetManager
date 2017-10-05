c459a493de3b96c9de2c7e640985769c2bf42859
Kenneth Hansen <kennethkh96@gmail.com>
2017-10-05 16:39:53 +0200

    Added unittests

---
+++ BudgetManager/BudgetManager.Test/BudgetTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BudgetManager.Controllers;
+using System.Web.Mvc;
+using BudgetManager.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace BudgetManager.Test
+{
+    [TestClass]
+    public class BudgetTest
+    {
+        [TestMethod]
+        public void Index_Returns_ActionResult()
+        {
+            BudgetController controller = new BudgetController();
+
+            var actual = controller.Index();
+
+            Assert.IsInstanceOfType(actual, typeof(ActionResult));
+        }
+
+        [TestMethod]
+        public void Create_Returns_Bad_Request_If_Errors()
+        {
+            BudgetController controller = new BudgetController();
+            controller.ModelState.AddModelError("fakeError", "fakeError");
+            var actual = controller.Create(new Budget());
+            Assert.IsInstanceOfType(actual, typeof(HttpStatusCodeResult));
+        }
+    }
+}
---
+++ BudgetManager/BudgetManager.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BudgetManager.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BudgetManager.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ee0cceb0-221c-4497-8ebe-8c0a51debfbe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
